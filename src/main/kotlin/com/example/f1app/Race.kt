d3d723ee1e5bd45a5bb1e6d0ed02e893ebadff23
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-05-29 23:31:07 +0200

    add basic datatypes tasks

--- src/main/kotlin/com/example/f1app/Race.kt
+++ src/main/kotlin/com/example/f1app/Race.kt
@@ -1,6 +1,9 @@
 package com.example.f1app
 
-class Race {
+class Race(
+    val numberOfLaps: Int,
+    var currentLap: Int = 0,
+) {
     companion object {
         const val PITSTOP_TIME = 5.0 // 5 minutes
     }
--- src/main/kotlin/com/example/f1app/participants/Driver.kt
+++ src/main/kotlin/com/example/f1app/participants/Driver.kt
@@ -6,4 +6,8 @@ class Driver(
     val name: String,
     var points: Int = 0,
     val uuid: UUID = UUID.randomUUID(),
-)
+) {
+    fun addPoints(newPoints: Int) {
+        points += newPoints
+    }
+}
