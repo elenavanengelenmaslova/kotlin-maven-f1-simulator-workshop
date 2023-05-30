eb2071681d719db7dad125dcf8738a23c7c93a16
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-05-30 08:34:05 +0200

    add enums

--- src/main/kotlin/com/example/f1app/Race.kt
+++ src/main/kotlin/com/example/f1app/Race.kt
@@ -1,5 +1,7 @@
 package com.example.f1app
 
+import com.example.f1app.participants.Driver
+import com.example.f1app.participants.RaceCar
 import com.example.f1app.participants.Team
 
 class Race(
@@ -7,7 +9,23 @@ class Race(
     val teams: List<Team>,
     var currentLap: Int = 0,
 ) {
+    private val raceResults: MutableList<Result> = mutableListOf()
+
+    data class Result(
+        val team: Team,
+        val driver: Driver,
+        val car: RaceCar,
+        var totalLapTime: Double = 0.0,
+        var fastestLap: Double = Double.MAX_VALUE,
+    )
+
     companion object {
         const val PITSTOP_TIME = 5.0 // 5 minutes
     }
+}
+
+enum class RaceEvent {
+    NORMAL,
+    BREAKDOWN,
+    COLLISION,
 }
